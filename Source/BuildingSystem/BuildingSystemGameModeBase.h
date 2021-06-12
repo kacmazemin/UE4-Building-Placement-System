779e4b5d623d3bcece28a3aacc9b2047740443f5
mehmetemin <kacmazemin72@gmail.com>
2021-06-12 13:22:10 +0300

    Initial commit

---
+++ Source/BuildingSystem/BuildingSystemGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BuildingSystemGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGSYSTEM_API ABuildingSystemGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
